5dd0ac9654f94a835e09527fb38a5aac73ee37ab
Maruf Maniruzzaman <kuasha@gmail.com>
2014-10-08 12:38:52 -0700

    Barebone project starter

---
+++ startbarebone.py
@@ -0,0 +1,12 @@
+"""
+ Copyright (C) 2014 Maruf Maniruzzaman
+ Website: http://cosmosframework.com
+ Author: Maruf Maniruzzaman
+ License :: OSI Approved :: MIT License
+"""
+
+from samples.barebone.cosmosmain import main
+
+
+if __name__ == "__main__":
+    main()
