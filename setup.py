c41717d2efcaebf4e5e4b4d5fc92dc15ffdd6be6
Maruf Maniruzzaman <kuasha@acm.org>
2014-11-30 12:10:35 -0800

    v0.01.051.00

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='cosmos',
-    version='0.01.050.00',
+    version='0.01.051.00',
     packages=['cosmos', 'cosmos.admin', 'cosmos.admin.samples', 'cosmos.datamonitor', 'cosmos.dataservice', 'cosmos.rbac', 'cosmos.schema',
               'cosmos.service', 'cosmos.processors', 'test'],
     url='http://cosmosframework.com',
