dfe1c0b89dd136af7c66f9cbe1fb622d939fc12c
Maruf Maniruzzaman <kuasha@gmail.com>
2016-03-30 20:57:37 -0700

    OAuth2 token creation and validation

--- cosmos/auth/oauth2.py
+++ cosmos/auth/oauth2.py
@@ -8,6 +8,10 @@
 import jwt, Crypto.PublicKey.RSA as RSA, datetime
 import uuid
 
+
+class ValidationException(Exception):
+    pass
+
 """
   FROM RFC 6749: http://tools.ietf.org/html/rfc6749
 
@@ -66,9 +70,9 @@ given_name  User’s first name. The application can display this value.
 iat         Issued at time. The time when the JWT was issued. The time is represented as the number of seconds from January 1, 1970 (1970-01-01T0:0:0Z) UTC until the time the token was issued.
 iss         Identifies the token issuer
 nbf         Not before time. The time when the token becomes effective. For the token to be valid, the current date/time must be greater than or equal to the Nbf value. The time is represented as the number of seconds from January 1, 1970 (1970-01-01T0:0:0Z) UTC until the time the token was issued.
-oid         Object identifier (ID) of the user object in Azure AD.
+oid         Object identifier (ID) of the user object in system.
 sub         Token subject identifier. This is a persistent and immutable identifier for the user that the token describes. Use this value in caching logic.
-tid         Tenant identifier (ID) of the Azure AD tenant that issued the token.
+tid         Tenant identifier (ID) of the tenant that issued the token.
 unique_name A unique identifier for that can be displayed to the user. This is usually a user principal name (UPN).
 upn         User principal name of the user.
 ver         Version. The version of the JWT token, typically 1.0.
@@ -76,11 +80,11 @@ ver         Version. The version of the JWT token, typically 1.0.
 """
 
 
-def get_token(user, code, **kwargs):
-    aud = None
-    exp = None
-    family_name = None
-    given_name = None
+def get_token(**kwargs):
+    aud = kwargs.get("aud", None)
+    exp = kwargs.get("exp", None)
+    family_name = kwargs.get("family_name", None)
+    given_name = kwargs.get("given_name", None)
 
     token_payload = {
         "aud": aud,
@@ -90,11 +94,48 @@ def get_token(user, code, **kwargs):
         "ver": "1.0"
     }
 
+    iss = kwargs.get("iss", None)
+    if iss:
+        token_payload["iss"] = iss
+
+    nbf = kwargs.get("nbf", None)
+    if nbf:
+        token_payload["nbf"] = nbf
+
+    oid = kwargs.get("oid", None)
+    if oid:
+        token_payload["oid"] = oid
+
+    sub = kwargs.get("sub", None)
+    if sub:
+        token_payload["sub"] = sub
+
+    tid = kwargs.get("tid", None)
+    if tid:
+        token_payload["tid"] = tid
+
+    unique_name = kwargs.get("unique_name", None)
+    if unique_name:
+        token_payload["unique_name"] = unique_name
+
+    upn = kwargs.get("upn", None)
+    if upn:
+        token_payload["upn"] = upn
+
     service_private_pem = kwargs.get("service_private_pem")
     if not service_private_pem:
         raise SystemError("service_private_pem is not defined")
 
     priv_key = RSA.importKey(service_private_pem)
-    token = jwt.generate_jwt(token_payload, priv_key, 'RS256', datetime.timedelta(minutes=5))
+    token = jwt.generate_jwt(token_payload, priv_key, 'RS256', exp)
 
     return token
+
+def verify_token(token, public_key_pem, alg_list):
+    try:
+        header, claims = jwt.verify_jwt(token, public_key_pem, alg_list)
+        return header, claims
+    except Exception as ex:
+        raise ValidationException(ex)
+
+
--- test/service/test_auth.py
+++ test/service/test_auth.py
@@ -6,7 +6,10 @@
 """
 from unittest import skip
 import unittest
+
 from cosmos.service.auth import get_hmac_password, validate_password, add_params, get_jwt_payload_json
+import cosmos.auth.oauth2
+import Crypto.PublicKey.RSA as RSA, datetime
 
 try:
     import urlparse  # py2
@@ -46,6 +49,66 @@ class RoleItemTest(LoggedTestCase):
         query = urlparse.parse_qs(parsed.query)
         self.failUnlessEqual(query["name"], ["id"])
         self.failUnlessEqual(query["value"], ["123"])
+
+    def test_oauth2_get_token_works(self):
+        key = RSA.generate(2048)
+        priv_pem = key.exportKey()
+        pub_pem = key.publickey().exportKey()
+
+        exp = datetime.timedelta(minutes=5)
+        payload = {"aud":"aud",
+                "family_name":"family_name",
+                "given_name":"given_name",
+                "iss":"iss",
+                "oid":"oid",
+                "sub":"sub",
+                "tid":"tid",
+                "unique_name":"unique_name",
+                "upn":"upn",
+        }
+
+        token = cosmos.auth.oauth2.get_token(aud="aud",
+                                     exp=exp,
+                                     family_name="family_name",
+                                     given_name="given_name",
+                                     iat="iat",
+                                     iss="iss",
+                                     nbf="nbf",
+                                     oid="oid",
+                                     sub="sub",
+                                     tid="tid",
+                                     unique_name="unique_name",
+                                     upn="upn",
+                                     service_private_pem = priv_pem
+                                     )
+
+        header, claims = cosmos.auth.oauth2.verify_token(token, pub_pem, ['RS256'])
+        self.failUnlessEqual(header.get("typ"), "JWT")
+        self.failUnlessEqual(header.get("alg"), "RS256")
+        for k in payload:
+            self.failUnlessEqual(claims[k], payload[k])
+
+    def test_oauth2_get_token_wrong_key_throws(self):
+        key = RSA.generate(2048)
+        key2 = RSA.generate(2048)
+        priv_pem = key.exportKey()
+        pub_pem = key2.publickey().exportKey()
+
+        exp = datetime.timedelta(minutes=5)
+
+        token = cosmos.auth.oauth2.get_token(aud="aud",
+                                     exp=exp,
+                                     family_name="family_name",
+                                     given_name="given_name",
+                                     service_private_pem = priv_pem
+                                     )
+        try:
+            cosmos.auth.oauth2.verify_token(token, pub_pem, ['RS256'])
+            self.fail("Should throw exception")
+        except cosmos.auth.oauth2.ValidationException as ve:
+            pass
+
+
 
 if __name__ == "__main__":
     unittest.main()
